5daa50e9cbaec2cb10ad29bdbd82666e4289d309
Ivan Grginov <ivan.grginov@infinum.hr>
2020-07-27 10:15:36 +0200

    Implemented http_client abstraction

---
+++ src/blocks/integrations/core/class-http-client.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Guzzle client, implementation of Http_Client.
+ *
+ * @package Eightshift_Forms\Integrations\Core
+ */
+
+declare( strict_types=1 );
+
+namespace Eightshift_Forms\Integrations\Core;
+
+use GuzzleHttp\ClientInterface;
+
+/**
+ * Guzzle client, implementation of Http_Client.
+ */
+class Guzzle_Client implements Http_Client {
+
+  /**
+   * Constructs object
+   *
+   * @param ClientInterface $client DI injected Guzzle client.
+   */
+  public function __construct( ClientInterface $client ) {
+    $this->client = $client;
+  }
+
+  /**
+   * Implementation of get request on the Http_Client.
+   *
+   * @param  string $url        Url to ping.
+   * @param  array  $parameters (Optional) parameters for the request
+   * @return mixed
+   */
+  public function get( string $url, array $parameters = array() ) {
+    return $this->client->get( $url, $parameters );
+  }
+}
