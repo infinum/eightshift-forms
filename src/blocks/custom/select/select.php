de65dc6111b4ba88847232af67e97c27a544017c
Ivan Grginov <ivan.grginov@infinum.hr>
2020-10-01 18:02:32 +0200

    Remove stray error_log

--- src/blocks/custom/select/select.php
+++ src/blocks/custom/select/select.php
@@ -9,7 +9,6 @@ namespace Eightshift_Forms\Blocks;
 
 use Eightshift_Forms\Helpers\Components;
 
-error_log(print_r($attributes, true));
 $attributes['innerBlockContent'] = $inner_block_content;
 
 echo \wp_kses_post( Components::render( 'select', $attributes ) );
