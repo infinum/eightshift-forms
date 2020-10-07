6a7d3e6572457d3260cd17a580764e322ec8afc2
Ivan Grginov <ivan.grginov@infinum.hr>
2020-10-07 11:18:36 +0200

    Added basic test setup

---
+++ tests/BuckarooTest.php
@@ -0,0 +1,19 @@
+<?php namespace EightshiftFormsTests;
+
+class BuckarooTest extends \Codeception\Test\Unit
+{
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    // tests
+    public function testSomeFeature()
+    {
+      $this->assertTrue(true);
+    }
+}
