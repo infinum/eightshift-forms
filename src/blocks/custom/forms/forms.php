deb96cc1c30f4923eed9f7e0305703e2d349040a
Ivan Grginov <ivan.grginov@infinum.hr>
2020-10-22 16:01:34 +0200

    Removed stray varexport

--- src/blocks/custom/forms/forms.php
+++ src/blocks/custom/forms/forms.php
@@ -10,15 +10,6 @@ namespace Eightshift_Forms\Blocks;
 use Eightshift_Forms\View\Form_View;
 use Eightshift_Forms\Helpers\Forms;
 
-function varexport($expression, $return=FALSE) {
-  $export = var_export($expression, TRUE);
-  $export = preg_replace("/^([ ]*)(.*)/m", '$1$1$2', $export);
-  $array = preg_split("/\r\n|\n|\r/", $export);
-  $array = preg_replace(["/\s*array\s\($/", "/\)(,)?$/", "/\s=>\s$/"], [NULL, ']$1', ' => ['], $array);
-  $export = join(PHP_EOL, array_filter(["["] + $array));
-  if ((bool)$return) return $export; else echo $export;
-}
-
 $block_class      = $attributes['blockClass'] ?? '';
 $selected_form_id = $attributes['selectedFormId'] ?? 0;
 $theme            = $attributes['theme'] ?? '';
@@ -30,10 +21,6 @@ if ( ! empty( $theme ) ) {
 } else {
   $post_blocks = parse_blocks( $post_content );
 }
-
-echo '<pre>';
-echo varexport( $post_blocks );
-echo '</pre>';
 
 foreach ( $post_blocks as $post_block ) {
   echo wp_kses( render_block( $post_block ), Form_View::allowed_tags() );
