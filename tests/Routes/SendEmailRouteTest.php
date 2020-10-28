da4d5d2b8bb82704bbc5ab50eabc123d36d587ad
Ivan Grginov <ivan.grginov@infinum.hr>
2020-10-28 12:50:07 +0100

    Modified SendEmailRoute tests a bit

--- tests/Routes/SendEmailRouteTest.php
+++ tests/Routes/SendEmailRouteTest.php
@@ -1,7 +1,6 @@
 <?php namespace EightshiftFormsTests\Routes;
 
 use Eightshift_Forms\Rest\Send_Email_Route;
-
 class SendEmailRouteTest extends BaseRouteTest
 {
   protected function getRouteName(): string {
@@ -20,12 +19,77 @@ class SendEmailRouteTest extends BaseRouteTest
       $this->route_endpoint::TO_PARAM => 'some value',
       $this->route_endpoint::SUBJECT_PARAM => 'some value',
       $this->route_endpoint::MESSAGE_PARAM => 'some value',
-      $this->route_endpoint::ADDITIONAL_HEADERS_PARAM => 'some value',
     ];
     $response = $this->route_endpoint->route_callback( $request );
 
     $this->verifyProperlyFormattedResponse($response);
     $this->assertEquals(200, $response->data['code'] );
     $this->assertSame( 1, did_action( self::WP_MAIL_ACTION ) );
+  }
+
+  /**
+   * Correct request should result in 200 response
+   *
+   * @return void
+   */
+  public function testRestCallSuccessfulWithPlaceholders()
+  {
+    $request = new \WP_REST_Request('GET', $this->route_endpoint->get_route_uri());
+    $request->params['GET'] = [
+      $this->route_endpoint::TO_PARAM => 'to param',
+      $this->route_endpoint::SUBJECT_PARAM => 'subject',
+      $this->route_endpoint::MESSAGE_PARAM => 'Message [[message]]',
+    ];
+
+    $response = $this->route_endpoint->route_callback( $request );
+
+    $this->verifyProperlyFormattedResponse($response);
+    $this->assertEquals(200, $response->data['code'] );
+    $this->assertSame( 1, did_action( self::WP_MAIL_ACTION ) );
+  }
+
+  /**
+   * If any of the required params (to, subject, message) is empty, wp_mail will fail.
+   *
+   * @return void
+   */
+  public function testRestCallFailsIfRequiredParamsEmpty()
+  {
+    $request = new \WP_REST_Request('GET', $this->route_endpoint->get_route_uri());
+    $request->params['GET'] = [
+      $this->route_endpoint::TO_PARAM => 'to param',
+      $this->route_endpoint::SUBJECT_PARAM => 'subject',
+      $this->route_endpoint::MESSAGE_PARAM => '',
+    ];
+
+    $response = $this->route_endpoint->route_callback( $request );
+
+    $this->verifyProperlyFormattedResponse($response);
+    $this->assertEquals(400, $response->data['code'] );
+    $this->assertSame( 0, did_action( self::WP_MAIL_ACTION ) );
+
+    $request->params['GET'] = [
+      $this->route_endpoint::TO_PARAM => 'to param',
+      $this->route_endpoint::SUBJECT_PARAM => '',
+      $this->route_endpoint::MESSAGE_PARAM => 'message',
+    ];
+
+    $response = $this->route_endpoint->route_callback( $request );
+
+    $this->verifyProperlyFormattedResponse($response);
+    $this->assertEquals(400, $response->data['code'] );
+    $this->assertSame( 0, did_action( self::WP_MAIL_ACTION ) );
+
+    $request->params['GET'] = [
+      $this->route_endpoint::TO_PARAM => '',
+      $this->route_endpoint::SUBJECT_PARAM => 'subject',
+      $this->route_endpoint::MESSAGE_PARAM => 'message',
+    ];
+
+    $response = $this->route_endpoint->route_callback( $request );
+
+    $this->verifyProperlyFormattedResponse($response);
+    $this->assertEquals(400, $response->data['code'] );
+    $this->assertSame( 0, did_action( self::WP_MAIL_ACTION ) );
   }
 }
