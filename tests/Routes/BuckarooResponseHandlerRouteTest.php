b3f7abc361fff1d86a01f5353b3c26bbd0a365c1
Ivan Grginov <ivan.grginov@infinum.hr>
2020-10-29 15:47:48 +0100

    Added tests for redirect URL setting

--- tests/Routes/BuckarooResponseHandlerRouteTest.php
+++ tests/Routes/BuckarooResponseHandlerRouteTest.php
@@ -4,9 +4,12 @@ use Eightshift_Forms\Integrations\Authorization\HMAC;
 use Eightshift_Forms\Rest\Buckaroo_Response_Handler_Route;
 use Eightshift_Forms\Hooks\Filters;
 use Eightshift_Forms\Hooks\Actions;
+use EightshiftFormsTests\Integrations\Buckaroo\DataProvider;
+use Brain\Monkey\Filters as BrainFilters;
 
 class BuckarooResponseHandlerRouteTest extends BaseRouteTest implements Filters, Actions
 {
+
   protected function getRouteName(): string {
     return Buckaroo_Response_Handler_Route::class;
   }
@@ -46,14 +49,12 @@ class BuckarooResponseHandlerRouteTest extends BaseRouteTest implements Filters,
       $this->route_endpoint::REDIRECT_URL_REJECT_PARAM => 'http://someurl-com',
       $this->route_endpoint::STATUS_PARAM => 'success',
     ];
-    $request->params['POST'] = [
-      $this->route_endpoint::BUCKAROO_RESPONSE_CODE_PARAM => 190,
-    ];
+    $request->params['POST'] = DataProvider::idealSuccessResponseMock();
     $request->params['GET'][ HMAC::AUTHORIZATION_KEY ] = $this->hmac->generate_hash($request->get_query_params(), \apply_filters( self::BUCKAROO, 'secret_key' ) );
     $response = $this->route_endpoint->route_callback( $request );
 
     $this->verifyProperlyFormattedResponse($response);
-    $this->assertEquals(200, $response->data['code'], $response->data['data']['message']);
+    $this->assertEquals(200, $response->data['code'], $response->data['data']['message'] ?? 'message not defined');
   }
 
   /**
@@ -123,12 +124,9 @@ class BuckarooResponseHandlerRouteTest extends BaseRouteTest implements Filters,
       $this->route_endpoint::REDIRECT_URL_REJECT_PARAM => 'http://someurl-com',
       $this->route_endpoint::STATUS_PARAM => 'success',
     ];
-    $request->params['POST'] = [
-      $this->route_endpoint::BUCKAROO_RESPONSE_CODE_PARAM => 190,
-    ];
+    $request->params['POST'] = $request->params['POST'] = DataProvider::idealSuccessResponseMock();
     $request->params['GET'][ HMAC::AUTHORIZATION_KEY ] = $this->hmac->generate_hash($request->get_query_params(), \apply_filters( self::BUCKAROO, 'secret_key' ) );
-    $response = $this->route_endpoint->route_callback( $request );
-
+    $this->route_endpoint->route_callback( $request );
     $this->assertSame( 1, did_action( self::WP_REDIRECT_ACTION ) );
   }
 
@@ -157,4 +155,152 @@ class BuckarooResponseHandlerRouteTest extends BaseRouteTest implements Filters,
 
     $this->assertSame( 1, did_action( self::WP_REDIRECT_ACTION ) );
   }
+
+  /**
+   * Test redirection works on iDEAL success
+   *
+   * @return void
+   */
+  public function testRedirectUrlBuildingWorksOnIdealSuccess()
+  {
+    $correct_url = 'http://success.com';
+    $params = [
+      $this->route_endpoint::STATUS_PARAM => $this->route_endpoint::STATUS_SUCCESS,
+      $this->route_endpoint::REDIRECT_URL_PARAM => $correct_url,
+    ];
+
+    $redirect_url = $this->route_endpoint->build_redirect_url( $params, DataProvider::idealSuccessResponseMock() );
+    $this->assertEquals($redirect_url, $correct_url);
+  }
+
+  /**
+   * Test redirection works on iDEAL error
+   *
+   * @return void
+   */
+  public function testRedirectUrlBuildingWorksOnIdeaelError()
+  {
+    $correct_url = 'http://error.com';
+    $params = [
+      $this->route_endpoint::STATUS_PARAM => $this->route_endpoint::STATUS_ERROR,
+      $this->route_endpoint::REDIRECT_URL_ERROR_PARAM => $correct_url,
+    ];
+
+    $redirect_url = $this->route_endpoint->build_redirect_url( $params, DataProvider::idealErrorResponseMock() );
+    $this->assertEquals($redirect_url, $correct_url);
+  }
+
+  /**
+   * Test redirection works on iDEAL reject
+   *
+   * @return void
+   */
+  public function testRedirectUrlBuildingWorksOnIdealReject()
+  {
+    $correct_url = 'http://reject.com';
+    $params = [
+      $this->route_endpoint::STATUS_PARAM => $this->route_endpoint::STATUS_REJECT,
+      $this->route_endpoint::REDIRECT_URL_REJECT_PARAM => $correct_url,
+    ];
+
+    $redirect_url = $this->route_endpoint->build_redirect_url( $params, DataProvider::idealRejectResponseMock() );
+    $this->assertEquals($redirect_url, $correct_url);
+  }
+
+  /**
+   * Test redirection works on iDEAL cancel
+   *
+   * @return void
+   */
+  public function testRedirectUrlBuildingWorksOnIdealCancel()
+  {
+    $correct_url = 'http://cancel.com';
+    $params = [
+      $this->route_endpoint::STATUS_PARAM => $this->route_endpoint::STATUS_CANCELED,
+      $this->route_endpoint::REDIRECT_URL_CANCEL_PARAM => $correct_url,
+    ];
+
+    $redirect_url = $this->route_endpoint->build_redirect_url( $params, DataProvider::idealCancelledResponseMock() );
+    $this->assertEquals($redirect_url, $correct_url);
+  }
+
+  /**
+   * Test redirection redirects back to homepage if url isn't provided.
+   *
+   * @return void
+   */
+  public function testRedirectUrlBuildingWorksWhenUrlNotProvided()
+  {
+    $params = [
+      $this->route_endpoint::STATUS_PARAM => $this->route_endpoint::STATUS_SUCCESS,
+    ];
+
+    $redirect_url = $this->route_endpoint->build_redirect_url( $params, DataProvider::idealSuccessResponseMock() );
+    $this->assertEquals($redirect_url, self::HOME_URL);
+  }
+
+
+  /**
+   * Test redirection works on Emandate success
+   *
+   * @return void
+   */
+  public function testRedirectUrlBuildingWorksOnEmandateSuccess()
+  {
+    $correct_url = 'http://success.com';
+    $params = [
+      $this->route_endpoint::STATUS_PARAM => $this->route_endpoint::STATUS_SUCCESS,
+      $this->route_endpoint::REDIRECT_URL_PARAM => $correct_url,
+    ];
+
+    $redirect_url = $this->route_endpoint->build_redirect_url( $params, DataProvider::emandateSuccessResponseMock() );
+    $this->assertEquals($redirect_url, $correct_url);
+  }
+
+  /**
+   * Test redirection works on Emandate error
+   *
+   * @return void
+   */
+  public function testRedirectUrlBuildingWorksOnEmandatelError()
+  {
+    $correct_url = 'http://error.com';
+    $params = [
+      $this->route_endpoint::STATUS_PARAM => $this->route_endpoint::STATUS_ERROR,
+      $this->route_endpoint::REDIRECT_URL_ERROR_PARAM => $correct_url,
+    ];
+
+    $redirect_url = $this->route_endpoint->build_redirect_url( $params, DataProvider::emandateFailedResponseMock() );
+    $this->assertEquals($redirect_url, $correct_url);
+  }
+
+  /**
+   * Test redirection works on Emandate cancel
+   *
+   * @return void
+   */
+  public function testRedirectUrlBuildingWorksOnEmandateCancel()
+  {
+    $correct_url = 'http://cancel.com';
+    $params = [
+      $this->route_endpoint::STATUS_PARAM => $this->route_endpoint::STATUS_CANCELED,
+      $this->route_endpoint::REDIRECT_URL_CANCEL_PARAM => $correct_url,
+    ];
+
+    $redirect_url = $this->route_endpoint->build_redirect_url( $params, DataProvider::emandateCancelledResponseMock() );
+    $this->assertEquals($redirect_url, $correct_url);
+  }
+
+  /**
+   * Test url was filtered if filter is set in project
+   *
+   * @return void
+   */
+  public function testRedirectUrlWasFilteredIfProvided()
+  {
+    apply_filters(Filters::BUCKAROO_REDIRECT_URL, 'Filter applied', $this);
+    $this->route_endpoint->build_redirect_url( [], DataProvider::emandateCancelledResponseMock() );
+    $this->assertTrue( BrainFilters\applied( Filters::BUCKAROO_REDIRECT_URL ) > 0 );
+  }
+
 }
