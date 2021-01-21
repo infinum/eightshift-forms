3d352948516ce850c7ce7e78a57262a6ec97c923
Ivan Grginov <ivan.grginov@infinum.hr>
2021-01-21 17:03:52 +0100

    Fixed phpcs errors

--- src/blocks/custom/form/form.php
+++ src/blocks/custom/form/form.php
@@ -58,7 +58,7 @@ $mailchimp_tags               = $attributes['mailchimpTags'] ?? [];
 $custom_event_names           = $attributes['eventNames'] ?? [];
 $used_types                   = Forms::detect_used_types( $is_form_complex, $form_type, $form_types_complex, $form_types_complex_redirect );
 $inner_block_content          = ! empty( $inner_block_content ) ? $inner_block_content : '';
-$form_id                      = 'form-' . hash( 'crc32', time() . mt_rand(0, 10000) );
+$form_id                      = 'form-' . hash( 'crc32', time() . wp_rand( 0, 10000 ) );
 
 $block_classes = Components::classnames([
   $block_class,
@@ -161,7 +161,7 @@ if ( empty( $this ) ) {
       <?php } ?>
     <?php } ?>
 
-    <input type="hidden" name="form-unique-id" value="<?php echo esc_attr( $form_id )?>" />
+    <input type="hidden" name="form-unique-id" value="<?php echo esc_attr( $form_id ); ?>" />
     <?php wp_nonce_field( $form_id, 'nonce', false ); ?>
   </form>
 
