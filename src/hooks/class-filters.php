6fbac750d6bc6a182c34db632956ae7734288ba9
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-02-15 11:34:11 +0100

    updating forms

--- src/hooks/class-filters.php
+++ src/hooks/class-filters.php
@@ -15,39 +15,19 @@ namespace Eightshift_Forms\Hooks;
 interface Filters {
 
   /**
-   * Filter used to provide Microsoft Dynamics CRM credentials / info from your project to Eightshift Forms.
-   *
-   * Expects an array to be returned with at least the following keys:
-   *  'client_id'
-   *  'client_secret'
-   *  'auth_token_url'
-   *  'scope'
-   *  'api_url'
-   *  'available_entities'
+   * Filter used to provide form themes.
    *
    * Example:
    *
    *   public function register(): void {
-   *     add_filter( 'eightshift_forms/dynamics_info', [ $this, 'get_info' ], 11, 1 );
+   *     add_filter( 'eightshift_forms/general_info', [ $this, 'getThemes' ], 11 );
    *   }
    *
-   *   public function get_info( string $key ) {
-   *     $available_entities = [
-   *       'entity1',
-   *       'entity2,
-   *        // ...
-   *     ];
-   *
-   *     $info = [
-   *       'client_id' => 'defined( 'ESF_DYNAMICS_CRM_CLIENT_ID' ) ? ESF_DYNAMICS_CRM_CLIENT_ID : '','
-   *       'client_secret' => defined( 'ESF_DYNAMICS_CRM_CLIENT_SECRET' ) ? ESF_DYNAMICS_CRM_CLIENT_SECRET : '',
-   *       'auth_token_url' => defined( 'ESF_DYNAMICS_CRM_AUTH_TOKEN_URL' ) ? ESF_DYNAMICS_CRM_AUTH_TOKEN_URL : '',
-   *       'scope' => defined( 'ESF_DYNAMICS_CRM_SCOPE' ) ? ESF_DYNAMICS_CRM_SCOPE : '',
-   *       'api_url' => defined( 'ESF_DYNAMICS_CRM_API_URL' ) ? ESF_DYNAMICS_CRM_API_URL : '',
-   *       'available_entities' => $available_entities,
-   *     ];
-   *
-   *     return $info[ $key ] ?? '';
+   *   public function getThemes(): array
+   *   {
+   *   	return [
+   *   		'Newsletter',
+   *   	];
    *   }
    *
    * @var string
