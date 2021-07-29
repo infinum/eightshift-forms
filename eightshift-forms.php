b76625296b3dee5b4bf43b72016ea3f1f1be4654
Ivan Grginov <ivan.grginov@infinum.com>
2021-07-29 12:40:32 +0200

    Update version

--- eightshift-forms.php
+++ eightshift-forms.php
@@ -6,7 +6,7 @@
  * Description: Eightshift form builder plugin.
  * Author: Team Eightshift
  * Author URI: https://eightshift.com/
- * Version: 0.2.3
+ * Version: 0.3.0
  * Text Domain: eightshift-forms
  *
  * @package EightshiftForms
