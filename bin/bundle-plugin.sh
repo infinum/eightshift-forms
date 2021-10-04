82157f6dfbca06ad02bc3320151cf8c3df6c80f9
Denis Žoljom <denis.zoljom@gmail.com>
2021-10-04 21:03:57 +0200

    Update bundle script

--- bin/bundle-plugin.sh
+++ bin/bundle-plugin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-set -x # debugging purposes
+#set -x # debugging purposes
 
 # Arguments
 # $1 - current_path variable (root)
@@ -9,14 +9,7 @@ set -x # debugging purposes
 mkdir -p ./eightshift-forms/
 
 # awk solution taken from https://stackoverflow.com/a/66832595/629127
-ignore_list=()
-while IFS='' read -r line; do
-  ignore_list+=("$line");
-done < <(awk -F/ 'index($0, "/plugin/") == 1 {sub(/ .*/, "", $NF); print $NF}' "$1"/.gitattributes)
-
-# add additional files that we don't want in our build
-ignore_list+=('node_modules')
-ignore_list+=('.DS_Store')
+ignore_list=(".DS_Store" "node_modules" ".git"  ".github"  "bin" ".storybook" "assets" "storybook" "tests" ".editorconfig" ".eslintignore"  ".eslintrc"  ".gitignore"  ".stylelintrc"  "babel.config.js" "composer.json" "composer.lock" "package.json" "package-lock.json" "phpcs.xml.dist" "phpstan.neon" "phpstan.neon.dist" "postcss.config.js" "webpack.config.js" "CODE_OF_CONDUCT.md" "codeception.yml" "travis.yml" "eightshift-forms")
 
 # Exclude the files and folders we don't want to keep
 for element in *; do
