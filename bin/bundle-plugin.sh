3e3cda4c9c9437306be239019563512925131b37
Denis Žoljom <denis.zoljom@gmail.com>
2021-10-04 21:17:47 +0200

    Debugging script

--- bin/bundle-plugin.sh
+++ bin/bundle-plugin.sh
@@ -9,7 +9,7 @@ set -euo pipefail
 mkdir -p ./eightshift-forms/
 
 # awk solution taken from https://stackoverflow.com/a/66832595/629127
-ignore_list=(".DS_Store" "node_modules" ".git"  ".github"  "bin" ".storybook" "assets" "storybook" "tests" ".editorconfig" ".eslintignore"  ".eslintrc"  ".gitignore"  ".stylelintrc"  "babel.config.js" "composer.json" "composer.lock" "package.json" "package-lock.json" "phpcs.xml.dist" "phpstan.neon" "phpstan.neon.dist" "postcss.config.js" "webpack.config.js" "CODE_OF_CONDUCT.md" "codeception.yml" "travis.yml" "eightshift-forms")
+ignore_list=(".DS_Store" "node_modules" ".git" ".github" "bin" ".storybook" "assets" "storybook" "tests" ".editorconfig" ".eslintignore" ".eslintrc" ".gitignore" ".stylelintrc" "babel.config.js" "composer.json" "composer.lock" "package.json" "package-lock.json" "phpcs.xml.dist" "phpstan.neon" "phpstan.neon.dist" "postcss.config.js" "webpack.config.js" "CODE_OF_CONDUCT.md" "codeception.yml" "travis.yml" "eightshift-forms")
 
 # Exclude the files and folders we don't want to keep
 for element in *; do
