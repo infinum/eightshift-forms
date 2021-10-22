0b8b7dadd0571920867aa867738aded33cb4be4a
gabriel-glo <gglogoski@gmail.com>
2021-10-22 15:42:03 +0200

    Add conditional for registering block categories

--- src/Blocks/Blocks.php
+++ src/Blocks/Blocks.php
@@ -56,7 +56,11 @@ class Blocks extends AbstractBlocks
 		remove_filter('the_content', 'wpautop');
 
 		// Create new custom category for custom blocks.
-		\add_filter('block_categories', [$this, 'getCustomCategoryOld'], 10, 2);
+		if (\is_wp_version_compatible('5.8')) {
+			\add_filter('block_categories_all', [$this, 'getCustomCategory'], 10, 2);
+		} else {
+			\add_filter('block_categories', [$this, 'getCustomCategoryOld'], 10, 2);
+		}
 
 		// Register blocks internal filter for props helper.
 		\add_filter(static::BLOCKS_DEPENDENCY_FILTER_NAME, [$this, 'getBlocksDataFullRawItem']);
