ec980bbd4c88e7968f74320c12c353d1e5ee15d5
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-10-24 21:57:35 +0200

    adding error msg if settings are wrong

--- src/Blocks/custom/greenhouse/greenhouse.php
+++ src/Blocks/custom/greenhouse/greenhouse.php
@@ -15,6 +15,8 @@ use EightshiftForms\Settings\Settings\SettingsGeneral;
 
 $manifest = Components::getManifest(__DIR__);
 
+$blockClass = $attributes['blockClass'] ?? '';
+
 $greenhouseServerSideRender = Components::checkAttr('greenhouseServerSideRender', $attributes, $manifest);
 $greenhouseFormPostId = Components::checkAttr('greenhouseFormPostId', $attributes, $manifest);
 
@@ -27,25 +29,33 @@ $greenhouseFormPostIdDecoded = Helper::encryptor('decode', $greenhouseFormPostId
 // Check if Greenhouse data is set and valid.
 $isSettingsValid = \apply_filters(SettingsGreenhouse::FILTER_SETTINGS_IS_VALID_NAME, $greenhouseFormPostIdDecoded);
 
-// Bailout if settings are not ok.
-if (!$isSettingsValid) {
-	return;
-}
+$greenhouseClass = Components::classnames([
+	Components::selector($blockClass, $blockClass),
+	Components::selector(!$isSettingsValid, $blockClass, '', 'invalid')
+]);
 
-echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	Greenhouse::FILTER_MAPPER_NAME,
-	[
-		'formPostId' => $greenhouseFormPostId,
-		'formType' => SettingsGreenhouse::SETTINGS_TYPE_KEY,
-		'formTrackingEventName' => \apply_filters(
-			SettingsAll::FILTER_BLOCK_SETTING_VALUE_NAME,
-			SettingsGeneral::SETTINGS_GENERAL_TRACKING_EVENT_NAME_KEY,
-			$greenhouseFormPostIdDecoded
-		),
-		'formSuccessRedirect' => \apply_filters(
-			SettingsAll::FILTER_BLOCK_SETTING_VALUE_NAME,
-			SettingsGeneral::SETTINGS_GENERAL_REDIRECTION_SUCCESS_KEY,
-			$greenhouseFormPostIdDecoded
-		),
-	]
-);
+// Bailout if settings are not ok.
+if ($isSettingsValid) {
+	echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		Greenhouse::FILTER_MAPPER_NAME,
+		[
+			'formPostId' => $greenhouseFormPostId,
+			'formType' => SettingsGreenhouse::SETTINGS_TYPE_KEY,
+			'formTrackingEventName' => \apply_filters(
+				SettingsAll::FILTER_BLOCK_SETTING_VALUE_NAME,
+				SettingsGeneral::SETTINGS_GENERAL_TRACKING_EVENT_NAME_KEY,
+				$greenhouseFormPostIdDecoded
+			),
+			'formSuccessRedirect' => \apply_filters(
+				SettingsAll::FILTER_BLOCK_SETTING_VALUE_NAME,
+				SettingsGeneral::SETTINGS_GENERAL_REDIRECTION_SUCCESS_KEY,
+				$greenhouseFormPostIdDecoded
+			),
+		]
+	);
+} else { ?>
+	<div class="<?php echo esc_attr($greenhouseClass); ?>">
+		<?php esc_html_e('Sorry, it looks like your Greenhose settings are not configured correctly. Please contact your admin.', 'eightshift-forms'); ?>
+	</div>
+		<?php
+}
