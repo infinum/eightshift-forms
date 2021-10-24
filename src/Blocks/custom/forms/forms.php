996b57c0685e609baa0c3f8295e6b218d34f1e4e
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-10-24 23:25:21 +0200

    adding cover image

--- src/Blocks/custom/forms/forms.php
+++ src/Blocks/custom/forms/forms.php
@@ -8,6 +8,7 @@
 
 use EightshiftForms\Helpers\Components;
 use EightshiftForms\Helpers\Helper;
+use EightshiftForms\Manifest\Manifest;
 
 $manifest = Components::getManifest(__DIR__);
 $globalManifest = Components::getManifest(dirname(__DIR__, 2));
@@ -19,37 +20,46 @@ $blockClass = $attributes['blockClass'] ?? '';
 // Check formPost ID prop.
 $formsFormPostId = Components::checkAttr('formsFormPostId', $attributes, $manifest);
 
+$formsClass = Components::classnames([
+	Components::selector($blockClass, $blockClass),
+	Components::selector(!$formsFormPostId, $blockClass, '', 'not-set')
+]);
+
 // Bailout if form post ID is missing.
-if (!$formsFormPostId) {
-	return;
-}
+if ($formsFormPostId) {
+	// Convert blocks to array.
+	$blocks = parse_blocks(get_the_content(null, null, $formsFormPostId));
 
-// Convert blocks to array.
-$blocks = parse_blocks(get_the_content(null, null, $formsFormPostId));
+	// Bailout if it fails for some reason.
+	if (!$blocks) {
+		return;
+	}
 
-// Bailout if it fails for some reason.
-if (!$blocks) {
-	return;
-}
+	// Encrypt.
+	$formsFormPostId = (string) Helper::encryptor('encrypt', $formsFormPostId);
 
-// Encrypt.
-$formsFormPostId = (string) Helper::encryptor('encrypt', $formsFormPostId);
+	// Iterate blocks an children by passing them form ID.
+	foreach ($blocks as $key => $block) {
+		if ($block['blockName'] === $globalManifest['namespace'] . '/form-selector') {
+			$blocks[$key]['attrs']['formSelectorFormPostId'] = $formsFormPostId;
 
-// Iterate blocks an children by passing them form ID.
-foreach ($blocks as $key => $block) {
-	if ($block['blockName'] === $globalManifest['namespace'] . '/form-selector') {
-		$blocks[$key]['attrs']['formSelectorFormPostId'] = $formsFormPostId;
-
-		foreach ($block['innerBlocks'] as $innerKey => $innerBlock) {
-			$name = Components::kebabToCamelCase(str_replace($globalManifest['namespace'] . '/', '', $innerBlock['blockName']));
-			$blockName = explode('/', $innerBlock['blockName'])[1];
-			$blocks[$key]['innerBlocks'][$innerKey]['attrs']["{$blockName}FormPostId"] = $formsFormPostId;
+			foreach ($block['innerBlocks'] as $innerKey => $innerBlock) {
+				$name = Components::kebabToCamelCase(str_replace($globalManifest['namespace'] . '/', '', $innerBlock['blockName']));
+				$blockName = explode('/', $innerBlock['blockName'])[1];
+				$blocks[$key]['innerBlocks'][$innerKey]['attrs']["{$blockName}FormPostId"] = $formsFormPostId;
+			}
 		}
 	}
-}
 
-// Render blocks.
-foreach ($blocks as $block) {
-	// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
-	echo \apply_filters('the_content', \render_block($block));
+	// Render blocks.
+	foreach ($blocks as $block) {
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo \apply_filters('the_content', \render_block($block));
+	}
+} else { ?>
+	<div class="<?php echo esc_attr($formsClass); ?>">
+		<img class="<?php echo esc_attr("{$blockClass}__image") ?>" src="<?php echo esc_url(\apply_filters(Manifest::MANIFEST_ITEM, 'cover.png')); ?>" />
+		<div class="<?php echo esc_attr("{$blockClass}__text") ?>"><?php esc_html_e('Please select form to show from the blocks sidebar.', 'eightshift-forms'); ?></div>
+	</div>
+		<?php
 }
