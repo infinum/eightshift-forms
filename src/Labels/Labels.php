5a4f9252b5c96432ecb98bee761e21b5d6e44e3a
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-11-10 10:27:02 +0100

    updating labels

--- src/Labels/Labels.php
+++ src/Labels/Labels.php
@@ -10,6 +10,10 @@ declare(strict_types=1);
 
 namespace EightshiftForms\Labels;
 
+use EightshiftForms\Integrations\Greenhouse\SettingsGreenhouse;
+use EightshiftForms\Integrations\Hubspot\SettingsHubspot;
+use EightshiftForms\Integrations\Mailchimp\SettingsMailchimp;
+use EightshiftForms\Integrations\Mailerlite\SettingsMailerlite;
 use EightshiftForms\Settings\SettingsHelper;
 
 /**
@@ -40,11 +44,55 @@ class Labels implements LabelsInterface
 	 */
 	public function getLabels(): array
 	{
-		return [
-			// Generic.
-			'submitWpError' => __('There was a problem with submitting your form. Please try again.', 'eightshift-forms'),
+		$output = array_merge(
+			$this->getGenericLabels(),
+			$this->getValidationLabels(),
+			$this->getMailerLabels()
+		);
 
-			// Validation.
+		// Greenhouse.
+		if ((bool) $this->isCheckboxOptionChecked(SettingsGreenhouse::SETTINGS_GREENHOUSE_USE_KEY, SettingsGreenhouse::SETTINGS_GREENHOUSE_USE_KEY)) {
+			$output = array_merge($output, $this->getGreenhouseLabels());
+		}
+
+		// Mailchimp.
+		if ((bool) $this->isCheckboxOptionChecked(SettingsMailchimp::SETTINGS_MAILCHIMP_USE_KEY, SettingsMailchimp::SETTINGS_MAILCHIMP_USE_KEY)) {
+			$output = array_merge($output, $this->getMailchimpLabels());
+		}
+
+		// Hubspot.
+		if ((bool) $this->isCheckboxOptionChecked(SettingsHubspot::SETTINGS_HUBSPOT_USE_KEY, SettingsHubspot::SETTINGS_HUBSPOT_USE_KEY)) {
+			$output = array_merge($output, $this->getHubspotLabels());
+		}
+
+		// Mailer.
+		if ((bool) $this->isCheckboxOptionChecked(SettingsMailerlite::SETTINGS_MAILERLITE_USE_KEY, SettingsMailerlite::SETTINGS_MAILERLITE_USE_KEY)) {
+			$output = array_merge($output, $this->getMailerliteLabels());
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Return labels - Generic
+	 *
+	 * @return array
+	 */
+	private function getGenericLabels(): array
+	{
+		return [
+			'submitWpError' => __('There was a problem with submitting your form. Please try again.', 'eightshift-forms'),
+		];
+	}
+
+	/**
+	 * Return labels - Validation
+	 *
+	 * @return array
+	 */
+	private function getValidationLabels(): array
+	{
+		return [
 			'validationRequired' => __('This field is required.', 'eightshift-forms'),
 			// translators: %s used for displaying required number.
 			'validationRequiredCount' => __('This field is required with minimum %s selected items.', 'eightshift-forms'),
@@ -58,15 +106,33 @@ class Labels implements LabelsInterface
 			'validationMinSize' => __('Your file is smaller than allowed. Minimum file size is %s kb.', 'eightshift-forms'),
 			// translators: %s used for displaying number value.
 			'validationMaxSize' => __('Your file is larget than allowed. Maximum file size is %s kb.', 'eightshift-forms'),
+		];
+	}
 
-			// Mailer.
+	/**
+	 * Return labels - Maler
+	 *
+	 * @return array
+	 */
+	private function getMailerLabels(): array
+	{
+		return [
 			'mailerSuccessNoSend' => __('Email sent successfully.', 'eightshift-forms'),
 			'mailerErrorSettingsMissing' => __('Mailer settings are not configured correctly. Please try again.', 'eightshift-forms'),
 			'mailerErrorEmailSend' => __('Email not sent due to unknown issue. Please try again.', 'eightshift-forms'),
 			'mailerErrorEmailConfirmationSend' => __('Email user confirmation not sent due to unknown issue. Please try again.', 'eightshift-forms'),
 			'mailerSuccess' => __('Email sent successfully.', 'eightshift-forms'),
+		];
+	}
 
-			// Greenhouse.
+	/**
+	 * Return labels - Greenhouse
+	 *
+	 * @return array
+	 */
+	private function getGreenhouseLabels(): array
+	{
+		return [
 			'greenhouseErrorSettingsMissing' => __('Greenhouse is not configured correctly. Please try again.', 'eightshift-forms'),
 			'greenhouseBadRequestError' => __('There is something wrong with your application. Please check all the fields and try again.', 'eightshift-forms'),
 			'greenhouseUnsupportedFileTypeError' => __('You have submitted an unsupported file type. Please try again.', 'eightshift-forms'),
@@ -85,23 +151,51 @@ class Labels implements LabelsInterface
 			'greenhouseInvalidFirstNameEmailPhoneError' => __('Your application has some invalid fields: first name, email and phone.', 'eightshift-forms'),
 			'greenhouseInvalidLastNameEmailPhoneError' => __('Your application has some invalid fields: last name, email and phone.', 'eightshift-forms'),
 			'greenhouseSuccess' => __('Your application is saved successfully. Thank you.', 'eightshift-forms'),
+		];
+	}
 
-			// Mailchimp.
+	/**
+	 * Return labels - Mailchimp
+	 *
+	 * @return array
+	 */
+	private function getMailchimpLabels(): array
+	{
+		return [
 			'mailchimpErrorSettingsMissing' => __('Mailchimp is not configured correctly. Please try again.', 'eightshift-forms'),
 			'mailchimpBadRequestError' => __('There is something wrong with your subscription. Please check all the fields and try again.', 'eightshift-forms'),
 			'mailchimpInvalidResourceError' => __('There is something wrong with your application. Please check all the fields and try again.', 'eightshift-forms'),
 			'mailchimpInvalidEmailError' => __('It looks like your email is not a valid format. Please try again.', 'eightshift-forms'),
 			'mailchimpMissingFieldsError' => __('It looks like we are missing some required fields. Please check all the fields and try again.', 'eightshift-forms'),
 			'mailchimpSuccess' => __('You have successfully subscribed to our newsletter. Thank you.', 'eightshift-forms'),
+		];
+	}
 
-			// HubSpot.
+	/**
+	 * Return labels - HubSpot
+	 *
+	 * @return array
+	 */
+	private function getHubspotLabels(): array
+	{
+		return [
 			'hubspotErrorSettingsMissing' => __('Hubspot is not configured correctly. Please try again.', 'eightshift-forms'),
 			'hubspotBadRequestError' => __('There is something wrong with your application. Please check all the fields and try again.', 'eightshift-forms'),
 			'hubspotInvalidRequestError' => __('There is something wrong with your application. Please check all the fields and try again.', 'eightshift-forms'),
 			'hubspotInvalidEmailError' => __('It looks like your email is not a valid format. Please try again.', 'eightshift-forms'),
 			'hubspotMissingFieldsError' => __('It looks like we are missing some required fields. Please check all the fields and try again.', 'eightshift-forms'),
 			'hubspotSuccess' => __('You have successfully submitted this form.  Thank you.', 'eightshift-forms'),
+		];
+	}
 
+	/**
+	 * Return labels - Mailerlite
+	 *
+	 * @return array
+	 */
+	private function getMailerliteLabels(): array
+	{
+		return [
 			// MailerLite.
 			'mailerliteErrorSettingsMissing' => __('Mailerlite is not configured correctly. Please try again.', 'eightshift-forms'),
 			'mailerliteBadRequestError' => __('There is something wrong with your subscription. Please check all the fields and try again.', 'eightshift-forms'),
