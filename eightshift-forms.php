0bf59c34c7c25d5bf2c82b889139fc81fe6faa1b
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2021-12-13 17:34:24 +0100

    updating webpack

--- eightshift-forms.php
+++ eightshift-forms.php
@@ -6,7 +6,7 @@
  * Description: Eightshift form builder plugin.
  * Author: Team Eightshift
  * Author URI: https://eightshift.com/
- * Version: 0.4.25-alpha
+ * Version: 0.4.26-alpha
  * Text Domain: eightshift-forms
  *
  * @package EightshiftForms
