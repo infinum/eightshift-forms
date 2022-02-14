211c65d569a3e6758995b5d71bf1f0350027d4d3
Ivan Ružević <ivan.ruzevic@infinum.hr>
2022-02-14 08:26:49 +0100

    Update src/Exception/MissingFilterInfoException.php
    Co-authored-by: Denis Žoljom <dingo-d@users.noreply.github.com>

--- src/Exception/MissingFilterInfoException.php
+++ src/Exception/MissingFilterInfoException.php
@@ -31,7 +31,7 @@ final class MissingFilterInfoException extends \InvalidArgumentException impleme
 		return new MissingFilterInfoException(
 			sprintf(
 				/* translators: %1$d is replaced with filter name, %2$d is replaced with filter type, , %3$d is replaced with name. */
-				\esc_html__('Filter for %1$s is missing or typed wrong. Provided type: %2$s, provided name: %3$s. Please check your name and try again.', 'eightshift-forms'),
+				\esc_html__('Filter for %1$s is missing or has a wrong type hint. Provided type: %2$s, provided name: %3$s. Please check your name and try again.', 'eightshift-forms'),
 				$filter,
 				$type,
 				$name
