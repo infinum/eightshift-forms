330c70cc9bf5c74adfb362e402f6e0ecbbf51237
Ivan Ruzevic <ivan.ruzevic@infinum.hr>
2022-02-18 12:06:44 +0100

    version bump

--- eightshift-forms.php
+++ eightshift-forms.php
@@ -6,7 +6,7 @@
  * Description: Eightshift form builder plugin.
  * Author: Team Eightshift
  * Author URI: https://eightshift.com/
- * Version: 0.7.2-alpha
+ * Version: 0.7.3-alpha
  * Text Domain: eightshift-forms
  *
  * @package EightshiftForms
