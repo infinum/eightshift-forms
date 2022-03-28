2abcea3d078d4cb704b006f1eabde99b6d6e3616
Ivan Ružević <ivan.ruzevic@infinum.hr>
2022-03-28 08:51:52 +0200

    Update src/Integrations/Clearbit/ClearbitClient.php
    Co-authored-by: Igor Obradović <igor.obradovic@infinum.com>

--- src/Integrations/Clearbit/ClearbitClient.php
+++ src/Integrations/Clearbit/ClearbitClient.php
@@ -294,7 +294,7 @@ class ClearbitClient implements ClearbitClientInterface
 	}
 
 	/**
-	 * Return Api Key from settings or global vairaible.
+	 * Return Api Key from settings or global variable.
 	 *
 	 * @return string
 	 */
