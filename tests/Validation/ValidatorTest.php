eecadd980cf0b3b2d4fcb941eb118da6f9b7fdd7
Mario Borna Mjertan <mario.borna.mjertan@infinum.com>
2022-04-11 16:24:37 +0200

    Set up Validator tests

---
+++ tests/Validation/ValidatorTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Tests\Unit\Validation;
+
+use Brain\Monkey;
+use Brain\Monkey\Functions;
+
+use function Tests\setupMocks;
+
+use EightshiftForms\Validation\Validator;
+use EightshiftForms\Labels\Labels;
+use EightshiftForms\Labels\LabelsInterface;
+use EightshiftForms\Validation\SettingsValidation;
+
+class ValidatorMock extends Validator {
+	public function __construct(LabelsInterface $labels) {
+		parent::__construct($labels);
+	}
+
+	public function getOptionValue(string $name): string {
+		switch($name) {
+			case 'validation-patterns': 
+				return "Numbers : [0-9]+\nLetters : [a-zA-Z]+\nInvalid-value";
+			default:
+				return $name;
+		}
+	}
+};
+
+
+/**
+ * Mock before tests.
+ */
+beforeEach(function () {
+	Monkey\setUp();
+	setupMocks();
+
+	$labels = new Labels();
+	$this->validator = new ValidatorMock($labels);
+});
+
+afterAll(function() {
+	Monkey\tearDown();
+});
