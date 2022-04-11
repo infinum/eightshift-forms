bc6001ff16a4c3050b9ff0115af69872630f3a14
Mario Borna Mjertan <mario.borna.mjertan@infinum.com>
2022-04-11 16:28:00 +0200

    Add basic test cases for Validator\validate

--- tests/Validation/ValidatorTest.php
+++ tests/Validation/ValidatorTest.php
@@ -106,3 +106,356 @@ test('getValidationPattern returns correct pattern for local patterns', function
 test('getValidationPattern returns name for non-existent pattern', function() {
 	expect($this->validator->getValidationPattern('I do not exist'))->toBe('I do not exist');
 });
+
+test('validate yields no false positives for basic validation', function ($expected, $params, $files, $formId, $formData) {
+	expect($this->validator->validate($params, $files, $formId, $formData))->toBe($expected);
+})->with([
+	[
+		[
+			'required-text-input' => 'This field is required.',
+		], // expected
+		[
+			'required-text-input' => [
+				'name' => 'required-text-input',
+				'value' => '',
+				'type' => 'text'
+			],
+		], // params
+		[], //files
+		'', // form ID
+		[ // form data
+			[
+				'component' => 'input',
+				'inputName' => 'required-text-input',
+				'inputId' => 'required-text-input',
+				'inputIsRequired' => true
+			],
+		],  
+	],
+	// ------ //
+	[
+		[
+			'text-min-length' => 'This field value has less characters than expected. We expect minimum 6 characters.',
+		],
+		[
+			'text-min-length' => [
+				'name' => 'text-min-length',
+				'value' => ':(',
+				'type' => 'text'
+			],
+		],
+		[], //files
+		'', // form ID,
+		[
+			[
+				'component' => 'input',
+				'inputName' => 'text-min-length',
+				'inputId' => 'text-min-length',
+				'inputIsRequired' => true,
+				'inputMinLength' => 6,
+			],
+		]
+	],
+	// ------ //
+	[
+		[
+			'text-max-length' => 'This field value has more characters than expected. We expect maximum 6 characters.',
+		],
+		[
+			'text-max-length' => [
+				'name' => 'text-max-length',
+				'value' => 'Oh no, this string is too large.',
+				'type' => 'text'
+			],
+		],
+		[],
+		'',
+		[
+			[
+				'component' => 'input',
+				'inputName' => 'text-max-length',
+				'inputId' => 'text-max-length',
+				'inputIsRequired' => true,
+				'inputMaxLength' => 6,
+			],
+		]
+	],
+	// ------ //
+	[
+		[
+			'number-passed-string' => 'This field should only contain numbers.',
+		],
+		[
+			'number-passed-string' => [
+				'name' => 'number-passed-string',
+				'value' => 'I am not a number.',
+				'type' => 'number'
+			],
+		],
+		[], 
+		'',
+		[
+			[
+				'component' => 'input',
+				'inputType' => 'number',
+				'inputIsNumber' => true,
+				'inputName' => 'number-passed-string',
+				'inputId' => 'number-passed-string',
+			]
+		],
+	],
+	// ------ //
+	[
+		[
+			'number-passed-string' => 'This field value has less characters than expected. We expect minimum 6 characters.',
+		],
+		[
+			'number-passed-string' => [
+				'name' => 'number-passed-string',
+				'value' => '12345',
+				'type' => 'number'
+			],
+		],
+		[], 
+		'',
+		[
+			[
+				'component' => 'input',
+				'inputType' => 'number',
+				'inputIsNumber' => true,
+				'inputName' => 'number-passed-string',
+				'inputId' => 'number-passed-string',
+				'inputMinLength' => 6
+			]
+		],
+	],
+	// ------ //
+	[
+		[
+			'number-max-length' => 'This field value has more characters than expected. We expect maximum 3 characters.',
+		],
+		[
+			'number-max-length' => [
+				'name' => 'number-max-length',
+				'value' => '1234',
+				'type' => 'number'
+			],
+		],
+		[],
+		'',
+		[
+			[
+				'component' => 'input',
+				'inputType' => 'number',
+				'inputIsNumber' => true,
+				'inputName' => 'number-max-length',
+				'inputId' => 'number-max-length',
+				'inputMaxLength' => 3,
+			],
+		]
+	],
+	// ------ //
+	[
+		[
+			'textarea' => 'This field is required.',
+		],
+		[
+			'textarea' => [
+				'name' => 'textarea',
+				'value' => '',
+				'type' => 'textarea'
+			],
+		],
+		[],
+		'',
+		[
+			[
+				'component' => 'textarea',
+				'textareaId' => 'textarea',
+				'textareaName' => 'textarea',
+				'textareaIsRequired' => true,
+			],
+		]
+	],
+	// ------ //
+	[
+		[],
+		[
+			'nonexistent-field' => []
+		],
+		[],
+		'', 
+		[
+			[
+				'component' => 'textarea',
+				'textareaId' => 'textarea',
+				'textareaName' => 'textarea',
+			]
+		]
+	],
+]);
+
+test('validate yields no false negatives for basic validation', function ($expected, $params, $files, $formId, $formData) {
+	expect($this->validator->validate($params, $files, $formId, $formData))->toBe($expected);
+})->with([
+	[
+		[
+		], // expected
+		[
+			'required-text-input' => [
+				'name' => 'required-text-input',
+				'value' => 'I have some input.',
+				'type' => 'text'
+			],
+		], // params
+		[], //files
+		'', // form ID
+		[ // form data
+			[
+				'component' => 'input',
+				'inputName' => 'required-text-input',
+				'inputId' => 'required-text-input',
+				'inputIsRequired' => true
+			],
+		],  
+	],
+	// ------ //
+	[
+		[
+		],
+		[
+			'text-min-length' => [
+				'name' => 'text-min-length',
+				'value' => 'This has more than six chars.',
+				'type' => 'text'
+			],
+		],
+		[], //files
+		'', // form ID,
+		[
+			[
+				'component' => 'input',
+				'inputName' => 'text-min-length',
+				'inputId' => 'text-min-length',
+				'inputIsRequired' => true,
+				'inputMinLength' => 6,
+			],
+		]
+	],
+	// ------ //
+	[
+		[
+		],
+		[
+			'text-max-length' => [
+				'name' => 'text-max-length',
+				'value' => 'works',
+				'type' => 'text'
+			],
+		],
+		[],
+		'',
+		[
+			[
+				'component' => 'input',
+				'inputName' => 'text-max-length',
+				'inputId' => 'text-max-length',
+				'inputIsRequired' => true,
+				'inputMaxLength' => 6,
+			],
+		]
+	],
+	// ------ //
+	[
+		[
+		],
+		[
+			'number-passed-string' => [
+				'name' => 'number-passed-string',
+				'value' => '42',
+				'type' => 'number'
+			],
+		],
+		[], 
+		'',
+		[
+			[
+				'component' => 'input',
+				'inputType' => 'number',
+				'inputIsNumber' => true,
+				'inputName' => 'number-passed-string',
+				'inputId' => 'number-passed-string',
+			]
+		],
+	],
+	// ------ //
+	[
+		[
+		],
+		[
+			'number-passed-string' => [
+				'name' => 'number-passed-string',
+				'value' => '123456',
+				'type' => 'number'
+			],
+		],
+		[], 
+		'',
+		[
+			[
+				'component' => 'input',
+				'inputType' => 'number',
+				'inputIsNumber' => true,
+				'inputName' => 'number-passed-string',
+				'inputId' => 'number-passed-string',
+				'inputMinLength' => 6
+			]
+		],
+	],
+	// ------ //
+	[
+		[
+		],
+		[
+			'number-max-length' => [
+				'name' => 'number-max-length',
+				'value' => '123',
+				'type' => 'number'
+			],
+		],
+		[],
+		'',
+		[
+			[
+				'component' => 'input',
+				'inputType' => 'number',
+				'inputIsNumber' => true,
+				'inputName' => 'number-max-length',
+				'inputId' => 'number-max-length',
+				'inputMaxLength' => 3,
+			],
+		]
+	],
+	// ------ //
+	[
+		[
+		],
+		[
+			'textarea' => [
+				'name' => 'textarea',
+				'value' => 'I have content.',
+				'type' => 'textarea'
+			],
+		],
+		[],
+		'',
+		[
+			[
+				'component' => 'textarea',
+				'textareaId' => 'textarea',
+				'textareaName' => 'textarea',
+				'textareaIsRequired' => true,
+			],
+		]
+	]
+]);
