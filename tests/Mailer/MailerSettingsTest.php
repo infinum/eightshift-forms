df196f7bc557b2d7edaf2537540bd92dcaf0c7b0
gabriel-glo <gglogoski@gmail.com>
2022-05-03 00:25:25 +0200

    Adding tests

--- tests/Mailer/MailerSettingsTest.php
+++ tests/Mailer/MailerSettingsTest.php
@@ -29,3 +29,17 @@ test('Register method will call sidebar hook', function () {
 	$this->assertSame(10, \has_filter(SettingsMailer::FILTER_SETTINGS_NAME, 'EightshiftForms\Mailer\SettingsMailer->getSettingsData()'), 'The callback getSettingsData should be hooked to custom filter hook with priority 10.');
 	$this->assertSame(10, \has_filter(SettingsMailer::FILTER_SETTINGS_IS_VALID_NAME, 'EightshiftForms\Mailer\SettingsMailer->isSettingsValid()'), 'The callback isSettingsValid should be hooked to custom filter hook with priority 10.');
 });
+
+test('isSettingsValid method returns correct values', function () {
+	$this->assertSame(true, $this->mailerSettings->isSettingsValid('1234'));
+});
+
+test('getSettingsSidebar method returns correct values', function () {
+	expect($this->mailerSettings->getSettingsSidebar())->toBeArray();
+	expect($this->mailerSettings->getSettingsSidebar())->toHaveKeys(['label', 'value', 'icon']);
+});
+
+test('getSettingsData method returns correct values', function () {
+	expect($this->mailerSettings->getSettingsData('1234'))->toBeArray();
+});
+
