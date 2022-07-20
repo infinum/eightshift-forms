ed77d3c9a12403f5ad6ebee64030f2cf0f77c0aa
Mario Borna Mjertan <mario.borna.mjertan@infinum.com>
2022-07-20 15:54:38 +0200

    Test blocks service class registration

--- tests/Blocks/BlocksTest.php
+++ tests/Blocks/BlocksTest.php
@@ -24,3 +24,21 @@ beforeEach(function () {
 afterEach(function() {
 	Monkey\tearDown();
 });
+
+test('Blocks service class registers hooks properly on WP >= 5.8', function () {
+	Actions\expectAdded('init');
+	Filters\expectRemoved('the_content', 'wpautop');
+	Filters\expectAdded('block_categories_all', [$this->blocks, 'getCustomCategory']);
+	Filters\expectAdded('es_blocks_string_to_filter', [$this->blocks, 'getStringToValue']);
+	Filters\expectAdded('es_blocks_options_checkbox_is_checked_filter', [$this->blocks, 'isCheckboxOptionChecked']);
+
+
+	$this->blocks->register();
+	
+	expect(has_action('init', [$this->blocks, 'getBlocksDataFullRaw']))->toBe(10);
+	expect(has_action('init', [$this->blocks, 'registerBlocks']))->toBe(11);
+	expect(has_filter('the_content', 'wpautop'))->toBe(false);
+	expect(has_filter('block_categories_all', [$this->blocks, 'getCustomCategory']))->toBe(10);
+	expect(has_filter('es_blocks_string_to_filter', [$this->blocks, 'getStringToValue']))->toBe(10);
+	expect(has_filter('es_blocks_options_checkbox_is_checked_filter', [$this->blocks, 'isCheckboxOptionChecked']))->toBe(10);
+});
