2359ae29172bcb240557b50b94acab9606f218b9
Ivan Ružević <ivan.ruzevic@infinum.com>
2023-02-27 12:25:30 +0100

    updating version

--- eightshift-forms.php
+++ eightshift-forms.php
@@ -6,7 +6,7 @@
  * Description: Eightshift Forms is a complete form builder plugin that utilizes modern Block editor features with multiple third-party integrations, bringing your project to a new level.
  * Author: WordPress team @Infinum
  * Author URI: https://eightshift.com/
- * Version: 4.0.19
+ * Version: 4.0.20
  * Text Domain: eightshift-forms
  *
  * @package EightshiftForms
