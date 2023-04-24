b4d848a253bb1e20779871cf3eaea600048634c4
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-04-24 13:50:31 +0200

    tweak form settings block

--- src/Blocks/components/utils/components/settings-button.js
+++ src/Blocks/components/utils/components/settings-button.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { __ } from '@wordpress/i18n';
 import { select } from '@wordpress/data';
-import { Button, BaseControl } from '@wordpress/components';
-import { icons } from '@eightshift/frontend-libs/scripts';
+import { Button } from '@wordpress/components';
+import { Control, icons } from '@eightshift/frontend-libs/scripts';
 import { getSettingsPageUrl } from './../index';
 
 export const SettingsButton = () => {
 	const postId = select('core/editor').getCurrentPostId();
 
 	return (
-		<BaseControl
-			help={__('On form settings page you can setup all additional settings regarding you form.', 'eightshift-forms')}
-		>
+		<Control>
 			<Button
 				href={getSettingsPageUrl(postId)}
-				variant="secondary"
 				icon={icons.options}
+				className='es-rounded-1 es-border-cool-gray-300 es-hover-border-cool-gray-400 es-transition'
 			>
-				{__('Open form settings', 'eightshift-forms')}
+				{__('Form settings', 'eightshift-forms')}
 			</Button>
-		</BaseControl>
+		</Control>
 	);
 };
