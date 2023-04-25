eb11115284c8dd6d4121ba48f6d53e8162dd9b2a
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2023-04-25 14:20:42 +0200

    simplify conditional tags editor code

--- src/Blocks/components/conditional-tags/components/conditional-tags-editor.js
+++ src/Blocks/components/conditional-tags/components/conditional-tags-editor.js
@@ -11,15 +11,15 @@ export const ConditionalTagsEditor = (attributes) => {
 
 	const conditionalTagsUse = checkAttr('conditionalTagsUse', attributes, manifest);
 
+	if (!conditionalTagsUse) {
+		return null;
+	}
+
 	return (
-		<>
-			{conditionalTagsUse &&
-				<div className='es-position-absolute es-right-2 es-top-0 es-nested-color-pure-white es-bg-cool-gray-650 es-nested-w-6 es-nested-h-6 es-w-10 es-h-10 es-rounded-full es-has-enhanced-contrast-icon es-display-flex es-items-center es-content-center'>
-					<Tooltip text={__('This field has conditional tags set', 'eightshift-forms')}>
-						{isFormPicker ? icons.visibilityAlt : icons.conditionalVisibility}
-					</Tooltip>
-				</div>
-			}
-		</>
+		<div className='es-position-absolute es-right-2 es-top-0 es-nested-color-pure-white es-bg-cool-gray-650 es-nested-w-6 es-nested-h-6 es-w-10 es-h-10 es-rounded-full es-has-enhanced-contrast-icon es-display-flex es-items-center es-content-center'>
+			<Tooltip text={__('This field has conditional tags set', 'eightshift-forms')}>
+				{isFormPicker ? icons.visibilityAlt : icons.conditionalVisibility}
+			</Tooltip>
+		</div>
 	);
 };
