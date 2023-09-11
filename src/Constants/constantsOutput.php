29dcfc89fa9cfcbd744b82725121422a26e38e7e
Ivan Ružević <ivan.ruzevic@infinum.com>
2023-09-11 14:18:42 +0200

    adding cloudflare geolocation

--- src/Constants/constantsOutput.php
+++ src/Constants/constantsOutput.php
@@ -9,13 +9,13 @@
  declare(strict_types=1);
 
 if (!defined('ES_GEOLOCATION_USE')) {
-	define('ES_GEOLOCATION_USE', true);
+	define('ES_GEOLOCATION_USE', false);
 }
 
 if (!defined('ES_GEOLOCATION_USE_WP_ROCKET')) {
-	define('ES_GEOLOCATION_USE_WP_ROCKET', true);
+	define('ES_GEOLOCATION_USE_WP_ROCKET', false);
 }
 
 if (!defined('ES_GEOLOCATION_USE_CLOUDFLARE')) {
-	define('ES_GEOLOCATION_USE_CLOUDFLARE', true);
+	define('ES_GEOLOCATION_USE_CLOUDFLARE', false);
 }
