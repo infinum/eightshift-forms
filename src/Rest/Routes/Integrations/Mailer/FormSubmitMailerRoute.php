59f62f5d927832728573d25f44180eca46393b21
Ivan Ružević <ivan.ruzevic@infinum.com>
2024-07-15 16:56:07 +0200

    hotfix

--- src/Rest/Routes/Integrations/Mailer/FormSubmitMailerRoute.php
+++ src/Rest/Routes/Integrations/Mailer/FormSubmitMailerRoute.php
@@ -85,10 +85,6 @@ class FormSubmitMailerRoute extends AbstractFormSubmit
 			}
 		}
 
-		$this->getFormSubmitMailer()->sendFallbackIntegrationEmail(
-			$formDetails
-		);
-
 		return \rest_ensure_response(
 			$this->getFormSubmitMailer()->sendEmails(
 				$formDetails,
