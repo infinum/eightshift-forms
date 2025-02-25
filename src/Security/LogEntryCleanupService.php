3b1a169b4a0cfa70ec174e16d314c88cdcaaad31
Mario Borna Mjertan <marioborna.mjertan@infobip.com>
2025-02-25 14:30:07 +0100

    Indent using tabs, not spaces
    Please add an .editorconfig to forms

--- src/Security/LogEntryCleanupService.php
+++ src/Security/LogEntryCleanupService.php
@@ -16,28 +16,28 @@ use EightshiftFormsVendor\EightshiftLibs\Services\ServiceInterface;
  * A log entry cleanup service.
  */
 class LogEntryCleanupService implements ServiceInterface {
-    public const LOG_ENTRY_CLEANUP_ACTION = 'es_forms_cleanup_log_entries';
+	public const LOG_ENTRY_CLEANUP_ACTION = 'es_forms_cleanup_log_entries';
 
-    public function register(): void {
-        add_action('init', [$this, 'maybeCleanupLogEntries']);
-        add_action(self::LOG_ENTRY_CLEANUP_ACTION, [$this, 'cleanupLogEntries']);
-    }
+	public function register(): void {
+		add_action('init', [$this, 'maybeCleanupLogEntries']);
+		add_action(self::LOG_ENTRY_CLEANUP_ACTION, [$this, 'cleanupLogEntries']);
+	}
 
-    public function maybeCleanupLogEntries() {
-        if (\function_exists('as_schedule_recurring_action')) {
-            \as_schedule_recurring_action(
-                time(),
-                DAY_IN_SECONDS,
-                self::LOG_ENTRY_CLEANUP_ACTION
-            );
+	public function maybeCleanupLogEntries() {
+		if (\function_exists('as_schedule_recurring_action')) {
+			\as_schedule_recurring_action(
+				time(),
+				DAY_IN_SECONDS,
+				self::LOG_ENTRY_CLEANUP_ACTION
+			);
 
-            return;
-        }
+			return;
+		}
 
-        $this->cleanupLogEntries();
-    }
+		$this->cleanupLogEntries();
+	}
 
-    public function cleanupLogEntries() {
-        RateLimitingLogEntry::cleanup(DAY_IN_SECONDS);
-    }
+	public function cleanupLogEntries() {
+		RateLimitingLogEntry::cleanup(DAY_IN_SECONDS);
+	}
 }
