697ca5f90994667bbf6ae88e60d096ba070e7c60
Goran Alković <77000136+goranalkovic-infinum@users.noreply.github.com>
2026-05-27 12:51:33 +0200

    tweak result output editor

--- src/Blocks/custom/result-output/components/result-output-editor.js
+++ src/Blocks/custom/result-output/components/result-output-editor.js
@@ -1,12 +1,17 @@
 import { form } from '@eightshift/ui-components/icons';
 import { __ } from '@wordpress/i18n';
-import { Placeholder } from '@wordpress/components';
+import { Notice } from '@eightshift/ui-components';
 
 export const ResultOutputEditor = () => {
 	return (
-		<Placeholder
-			icon={form}
-			label={<span>{__('Eightshift Forms - Result output', 'eightshift-forms')}</span>}
-		/>
+		<div className='esf:p-8 es:font-sans'>
+			<Notice
+				type='placeholder'
+				icon={form}
+				label={__('Eightshift Forms', 'eightshift-forms')}
+				subtitle={__('Result output', 'eightshift-forms')}
+				className='esf:w-fit'
+			/>
+		</div>
 	);
 };
