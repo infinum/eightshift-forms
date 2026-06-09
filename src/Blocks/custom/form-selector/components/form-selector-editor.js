88e89734bd32c0ad65980ba34c939261f6de8af1
Ivan Ružević <ivan.ruzevic@infinum.com>
2026-06-09 16:46:28 +0200

    updating examples

--- src/Blocks/custom/form-selector/components/form-selector-editor.js
+++ src/Blocks/custom/form-selector/components/form-selector-editor.js
@@ -16,70 +16,67 @@ export const FormSelectorEditor = ({ clientId, hasInnerBlocks }) => {
 		className: 'esf:max-w-3xl esf:mx-auto',
 	});
 
-	if (!hasInnerBlocks) {
-		return (
-			<div {...blockProps}>
-				<VStack
-					className='esf:items-center esf:py-8 es:font-sans'
-					noWrap
-				>
-					<RichLabel
-						icon={form}
-						label={__('Eightshift Forms', 'eightshift-ui-kit')}
-						className='esf:mb-24 esf:not-contrast-more:opacity-60'
-					/>
-
-					{forms.length > 0 && (
-						<RichLabel
-							labelClassName='esf:text-lg!'
-							label={__('Create a form', 'eightshift-ui-kit')}
-						/>
-					)}
-
-					{forms.length < 1 && (
-						<VStack className='esf:max-w-xs esf:items-center esf:gap-8 esf:text-center'>
-							<RichLabel
-								icon={warningCircle}
-								label={__('No integrations are active, configure one in the Dashboard', 'eightshift-forms')}
-								contentsOnly
-							/>
-
-							<Spacer />
-
-							<DashboardButton />
-						</VStack>
-					)}
-
-					<HStack className='esf:max-w-2xs esf:justify-center'>
-						{forms?.map((option) => {
-							const { label, slug, icon } = option;
-
-							let iconComponent = icon;
-
-							if (!icon) {
-								iconComponent = <JsxSvg svg={getUtilsIcons(camelCase(slug))} />;
-							}
-
-							return (
-								<Button
-									key={slug}
-									icon={iconComponent}
-									onPress={() => createBlockFromTemplate(clientId, slug, forms)}
-									size='large'
-									className='esf:size-80! esf:flex-col'
-								>
-									{label}
-								</Button>
-							);
-						})}
-					</HStack>
-				</VStack>
-			</div>
-		);
-	}
-
 	return (
 		<>
+			{!hasInnerBlocks && (
+				<div {...blockProps}>
+					<VStack
+						className='esf:items-center esf:py-8 es:font-sans'
+						noWrap
+					>
+						<RichLabel
+							icon={form}
+							label={__('Eightshift Forms', 'eightshift-ui-kit')}
+							className='esf:mb-24 esf:not-contrast-more:opacity-60'
+						/>
+
+						{forms.length > 0 && (
+							<RichLabel
+								labelClassName='esf:text-lg!'
+								label={__('Create a form', 'eightshift-ui-kit')}
+							/>
+						)}
+
+						{forms.length < 1 && (
+							<VStack className='esf:max-w-xs esf:items-center esf:gap-8 esf:text-center'>
+								<RichLabel
+									icon={warningCircle}
+									label={__('No integrations are active, configure one in the Dashboard', 'eightshift-forms')}
+									contentsOnly
+								/>
+
+								<Spacer />
+
+								<DashboardButton />
+							</VStack>
+						)}
+
+						<HStack className='esf:max-w-2xs esf:justify-center'>
+							{forms?.map((option) => {
+								const { label, slug, icon } = option;
+
+								let iconComponent = icon;
+
+								if (!icon) {
+									iconComponent = <JsxSvg svg={getUtilsIcons(camelCase(slug))} />;
+								}
+
+								return (
+									<Button
+										key={slug}
+										icon={iconComponent}
+										onPress={() => createBlockFromTemplate(clientId, slug, forms)}
+										size='large'
+										className='esf:size-80! esf:flex-col'
+									>
+										{label}
+									</Button>
+								);
+							})}
+						</HStack>
+					</VStack>
+				</div>
+			)}
 			<Toaster
 				richColors
 				position='bottom-center'
