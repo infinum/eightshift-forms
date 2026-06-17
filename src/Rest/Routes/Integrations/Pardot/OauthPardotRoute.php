355ce56dcf4048babb44f49d3d95837702f5e15a
Ivan Ružević <ivan.ruzevic@infinum.com>
2026-06-17 12:22:52 +0200

    feat: add Pardot REST routes (OAuth callback, items, form submit, test API)

---
+++ src/Rest/Routes/Integrations/Pardot/OauthPardotRoute.php
@@ -0,0 +1,117 @@
+<?php
+
+/**
+ * OAuth callback route for Pardot.
+ *
+ * @package EightshiftForms\Rest\Routes\Integrations\Pardot
+ */
+
+declare(strict_types=1);
+
+namespace EightshiftForms\Rest\Routes\Integrations\Pardot;
+
+use EightshiftForms\Integrations\Pardot\SettingsPardot;
+use EightshiftForms\Oauth\OauthInterface;
+use EightshiftForms\Rest\Routes\AbstractOauth;
+
+/**
+ * Class OauthPardotRoute
+ */
+class OauthPardotRoute extends AbstractOauth
+{
+	/**
+	 * Route slug.
+	 */
+	public const ROUTE_SLUG = 'pardot';
+
+	/**
+	 * Instance variable for Oauth.
+	 *
+	 * @var OauthInterface
+	 */
+	protected $oauthPardot;
+
+	/**
+	 * Create a new instance that injects classes
+	 *
+	 * @param OauthInterface $oauthPardot Inject Oauth methods.
+	 */
+	public function __construct(OauthInterface $oauthPardot)
+	{
+		$this->oauthPardot = $oauthPardot;
+	}
+
+	/**
+	 * Get the base url of the route
+	 *
+	 * @return string The base URL for route you are adding.
+	 */
+	protected function getRouteName(): string
+	{
+		return '/' . AbstractOauth::ROUTE_PREFIX_OAUTH_API . '/' . self::ROUTE_SLUG;
+	}
+
+	/**
+	 * Get the oauth type.
+	 *
+	 * @return string
+	 */
+	protected function getOauthType(): string
+	{
+		return SettingsPardot::SETTINGS_TYPE_KEY;
+	}
+
+	/**
+	 * Get the oauth allow key.
+	 *
+	 * @return string
+	 */
+	protected function getOauthAllowKey(): string
+	{
+		return SettingsPardot::SETTINGS_PARDOT_OAUTH_ALLOW_KEY;
+	}
+
+	/**
+	 * Check if the route is admin protected.
+	 *
+	 * @return boolean
+	 */
+	protected function isRouteAdminProtected(): bool
+	{
+		return false;
+	}
+
+	/**
+	 * Get mandatory params.
+	 *
+	 * @param array<string, mixed> $params Params passed from the request.
+	 *
+	 * @return array<string, string>
+	 */
+	protected function getMandatoryParams(array $params): array
+	{
+		return [];
+	}
+
+	/**
+	 * Implement submit action.
+	 *
+	 * @param string $code The code.
+	 *
+	 * @return mixed
+	 */
+	protected function submitAction(string $code)
+	{
+		$response = $this->oauthPardot->getAccessToken($code);
+
+		if ($response) {
+			$this->redirect(
+				\esc_html__('Oauth connection successful', 'eightshift-forms'),
+			);
+		}
+
+		$this->redirect(
+			\esc_html__('Oauth connection failed', 'eightshift-forms'),
+		);
+	}
+}
---
+++ src/Rest/Routes/Integrations/Pardot/TestApiPardotRoute.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * Test API route for Pardot.
+ *
+ * @package EightshiftForms\Rest\Routes\Integrations\Pardot
+ */
+
+declare(strict_types=1);
+
+namespace EightshiftForms\Rest\Routes\Integrations\Pardot;
+
+use EightshiftForms\Config\Config;
+use EightshiftForms\Exception\BadRequestException;
+use EightshiftForms\Integrations\Pardot\PardotClientInterface;
+use EightshiftForms\Integrations\Pardot\SettingsPardot;
+use EightshiftForms\Labels\LabelsInterface;
+use EightshiftForms\Rest\Routes\AbstractBaseRoute;
+use EightshiftForms\Rest\Routes\AbstractSimpleFormSubmit;
+use EightshiftForms\Security\SecurityInterface;
+use EightshiftForms\Validation\ValidatorInterface;
+use EightshiftFormsVendor\EightshiftLibs\Rest\Routes\AbstractRoute;
+
+/**
+ * Class TestApiPardotRoute
+ */
+class TestApiPardotRoute extends AbstractSimpleFormSubmit
+{
+	/**
+	 * Route slug.
+	 */
+	public const ROUTE_SLUG = SettingsPardot::SETTINGS_TYPE_KEY;
+
+	/**
+	 * Instance variable for Pardot data.
+	 *
+	 * @var PardotClientInterface
+	 */
+	protected $pardotClient;
+
+	/**
+	 * Create a new instance that injects classes
+	 *
+	 * @param SecurityInterface $security Inject security methods.
+	 * @param ValidatorInterface $validator Inject validator methods.
+	 * @param LabelsInterface $labels Inject labels methods.
+	 * @param PardotClientInterface $pardotClient Inject Pardot client.
+	 */
+	public function __construct(
+		SecurityInterface $security,
+		ValidatorInterface $validator,
+		LabelsInterface $labels,
+		PardotClientInterface $pardotClient
+	) {
+		$this->security = $security;
+		$this->validator = $validator;
+		$this->labels = $labels;
+		$this->pardotClient = $pardotClient;
+	}
+
+	/**
+	 * Get the base url of the route
+	 *
+	 * @return string The base URL for route you are adding.
+	 */
+	protected function getRouteName(): string
+	{
+		return '/' . Config::ROUTE_PREFIX_TEST_API . '/' . self::ROUTE_SLUG;
+	}
+
+	/**
+	 * Get mandatory params.
+	 *
+	 * @param array<string, mixed> $params Params passed from the request.
+	 *
+	 * @return array<string, string>
+	 */
+	protected function getMandatoryParams(array $params): array
+	{
+		return [
+			'type' => 'string',
+		];
+	}
+
+	/**
+	 * Check if the route is admin protected.
+	 *
+	 * @return boolean
+	 */
+	protected function isRouteAdminProtected(): bool
+	{
+		return true;
+	}
+
+	/**
+	 * Implement submit action.
+	 *
+	 * @param array<string, mixed> $params Prepared params.
+	 *
+	 * @throws BadRequestException If Pardot is not configured.
+	 *
+	 * @return array<string, mixed>
+	 */
+	protected function submitAction(array $params): array
+	{
+		$output = $this->pardotClient->getTestApi();
+
+		if ($output[Config::IARD_STATUS] === AbstractRoute::STATUS_ERROR) {
+			// phpcs:disable Eightshift.Security.HelpersEscape.ExceptionNotEscaped
+			throw new BadRequestException(
+				$this->getLabels()->getLabel('testApiError'),
+				[
+					AbstractBaseRoute::R_DEBUG => $output,
+					AbstractBaseRoute::R_DEBUG_KEY => 'testApiError',
+				]
+			);
+			// phpcs:enable
+		}
+
+		return [
+			AbstractBaseRoute::R_MSG => $this->getLabels()->getLabel('testApiSuccess'),
+			AbstractBaseRoute::R_DEBUG => [
+				AbstractBaseRoute::R_DEBUG => $output,
+				AbstractBaseRoute::R_DEBUG_KEY => 'testApiSuccess',
+			],
+		];
+	}
+}
