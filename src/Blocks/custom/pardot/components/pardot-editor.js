f08193efac8492422c939dd6bf277ad01a87dade
Ivan Ružević <ivan.ruzevic@infinum.com>
2026-07-03 08:50:52 +0200

    updating

--- src/Blocks/custom/pardot/components/pardot-editor.js
+++ src/Blocks/custom/pardot/components/pardot-editor.js
@@ -1,5 +1,5 @@
 import { InnerBlocks } from '@wordpress/block-editor';
-import { props, BlockInserter } from '@eightshift/frontend-libs/scripts';
+import { props, BlockInserter } from '@eightshift/frontend-libs-tailwind/scripts';
 import { FormEditor, additionalBlocksNoIntegration } from '../../../components/form/components/form-editor';
 
 export const PardotEditor = ({ attributes, setAttributes, clientId }) => {
